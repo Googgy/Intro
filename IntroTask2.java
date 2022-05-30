b002864c07a496b49c411cec0d223d011bc1fec0
Andrei Salenkou <nl5kcrash@gmail.com>
2022-05-30 18:49:45 +0300

    formatting

--- IntroTask2.java
+++ IntroTask2.java
@@ -1,8 +1,6 @@
 public class IntroTask2 {
-    
     public static void main (String[] args) {
         System.out.println ("Java: version 11.");
 	System.out.println ("JVM: Hotspot");
-}
-  
+    }
 }
